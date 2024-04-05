b3e85d023161eb867e130ba08437b787d721b84a
Uroš Mlakar <uros88mlakar@gmail.com>
2024-04-05 08:58:19 +0200

    Posodobil ogrodje naloge za študente

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ORV-SledenjeGibanju
 Sledenje gibanju
+
+V tem repozitoriju najdete okostje implementacije 4. naloge pri predmetu Osnove računalniškega vida. Strogo se držite predpisane strukture projekta.
+Koda v primerih je preizkušena na OS Linux.
--- naloga4.py
+++ naloga4.py
@@ -1,11 +1,20 @@
 import cv2 as cv
 import numpy as np
 
-def meanshift(slika, lokacija_okna, iteracije, napaka):
+def camshift(slika, sablone, iteracije, napaka):
+    pass
+
+def zaznaj_gibanje(cap, st_objektov=1):
+    '''Implemenacija zaznavanja gibanja, kjer poiščemo število premikajočih objektov glede na parameter st_objektov.'''
+    pass
+
+def izracunaj_znacilnice(lokacije_oken, prva_slika):
+    '''Izračunaj značilnice za sledenje iz prvega okna.'''
     pass
 
 if __name__ == "__main__":
     #Naloži video
+    zaznaj_gibanje = "rocno"
     cap = cv.VideoCapture('video.mp4')
     #Preveri, če je video uspešno naložen
     if not cap.isOpened():
@@ -16,20 +25,34 @@ if __name__ == "__main__":
     iteracije = 10
     napaka = 1
 
-    #Začetna točka sledenja
-    x, y, w, h = 0, 0, 0, 0
+    lokacije_oken = list()
+    #Začetna točka sledenja ročno
+    #(x, y, w, h)
+    if zaznaj_gibanje == "rocno":
+        lokacije_oken.append((0, 0, 0, 0))
+        lokacije_oken.append((0, 0, 0, 0))
+    else:
+        #Začetna točka sledenja avtomatsko
+        lokacije_oken = zaznaj_gibanje(cap, st_objektov=2)
+    
     #Izračun značilnic za sledenje
-    uspel, sablona = cap.read()
+    uspel, prva_slika = cap.read()
+    sablone = izracunaj_znacilnice(lokacije_oken,prva_slika)
+
 
     #Začetek sledenja
     while True:
         uspel, slika = cap.read()
         if not uspel:
             break        
-        cv.imshow('Slika', slika)
-        okno = meanshift(slika, (x, y, w, h), 10, 1)
+        
+        lokacije_novih_oken = camshift(slika, sablone,lokacije_oken, iteracije, napaka)
+        lokacije_oken = lokacije_novih_oken
         #Nariši okno
-        cv.rectangle(slika, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        for okno in lokacije_novih_oken:
+            x,y,w,h = okno
+            cv.rectangle(slika, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
+        cv.imshow('Rezultat', slika)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
